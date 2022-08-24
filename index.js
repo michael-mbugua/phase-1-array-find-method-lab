960b7414de47e029d2f947dac366d014766da0e4
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-08-24 17:23:42 +0300

    completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+const record = [
+    { year: "2018", result: "N/A" },
+    { year: "2017", result: "L" },
+    { year: "2013", result: "W" },
+    { year: "2016", result: "N/A" },
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+];
+
+function superbowlWin(record) {
+    for (let i of record) {
+    if (i.result === "W") {
+        return i.year;
+    }
+    }
+}
+record.find(superbowlWin)
